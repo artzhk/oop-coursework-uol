4aaae527b5422dec3182934c6da7b39a711ef938
Artem3124 <mr.zhukovsky02@gmail.com>
2024-10-22 22:40:12 +0200

    feat(menu): refactor

--- main.cpp
+++ main.cpp
@@ -1,54 +1,56 @@
 #include <iostream>
 #include <memory>
 
+using namespace std;
+
 class Menu {
 private:
-  std::unique_ptr<int> currentChoice;
+  unique_ptr<int> current_choice;
 
 public:
+  Menu() : current_choice(new int(0)) {}
 
-  Menu() : currentChoice(new int(0)) {}
+  void set_choice(int value) { *current_choice = value; }
 
-  void setChoice(int value) { *currentChoice = value; }
+  int get_choice() const { return *current_choice; }
 
-  int getChoice() const { return *currentChoice; }
-
-  void requestChoice() { std::cin >> *currentChoice; }
+  void request_choice() { cin >> *current_choice; }
 
   void render() const {
-    std::cout << "==Main Menu==" << std::endl;
-    std::cout << "1. Print help\n";
-    std::cout << "2. Print exchange stats\n";
-    std::cout << "3. Place an ask\n";
-    std::cout << "4. Place a bid\n";
-    std::cout << "5. Print wallet\n";
-    std::cout << "6. Continue\n";
-    std::cout << "Enter your choice (1-6): ";
+    cout << "==Main Menu==" << endl;
+    cout << "1. Print help\n";
+    cout << "2. Print exchange stats\n";
+    cout << "3. Place an ask\n";
+    cout << "4. Place a bid\n";
+    cout << "5. Print wallet\n";
+    cout << "6. Continue\n";
+    cout << "Enter your choice (1-6): ";
   }
 
-  void handleChoice() const {
-    switch (*currentChoice) {
+  void handle_choice() const {
+    cout << "\n\nYou selected: " << *current_choice << endl;
+    switch (*current_choice) {
     case 1:
       std::cout << "Help: This is a simple trading application. Select options "
                    "from the menu to interact.\n";
       break;
     case 2:
-      std::cout << "Exchange stats: No data available right now.\n";
+      cout << "Exchange stats: No data available right now.\n";
       break;
     case 3:
-      std::cout << "Place ask: Enter the amount you'd like to sell.\n";
+      cout << "Place ask: Enter the amount you'd like to sell.\n";
       break;
     case 4:
-      std::cout << "Place bid: Enter the amount you'd like to buy.\n";
+      cout << "Place bid: Enter the amount you'd like to buy.\n";
       break;
     case 5:
-      std::cout << "Wallet: Your current balance is $1000.\n";
+      cout << "Wallet: Your current balance is $1000.\n";
       break;
     case 6:
-      std::cout << "Continuing...\n";
+      cout << "Continuing...\n";
       break;
     default:
-      std::cout << "Invalid choice! Please select a number between 1 and 6.\n";
+      cout << "Invalid choice! Please select a number between 1 and 6.\n";
       break;
     }
   }
@@ -56,18 +58,18 @@ public:
 
 class Application {
 private:
-  std::unique_ptr<Menu> menu;
+  unique_ptr<Menu> menu;
 
 public:
   Application() : menu(new Menu()) {}
 
   void run() {
-    while (true) {
-      int choice = 0;
+    int x{1};
+    while (x) {
       menu->render();
-      std::cin >> choice;
-      menu->setChoice(choice);
-      menu->handleChoice();
+      menu->request_choice();
+      menu->handle_choice();
+      x--;
     }
   }
 };
