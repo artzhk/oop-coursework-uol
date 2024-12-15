11d6a1d79f132fd74b7e6a3bf8c16c0bb746b1fc
Artem3124 <mr.zhukovsky02@gmail.com>
2024-12-15 19:35:37 +0100

    refactor(utils): file_reader

--- utils/file_reader.cpp
+++ utils/file_reader.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-vector<string> FileReader::read_file(string path) {
+vector<string> FileReader::read_file(const string &path) {
   ifstream csv_file{path};
   vector<string> lines;
   string line;
@@ -22,22 +22,22 @@ vector<string> FileReader::read_file(string path) {
   return lines;
 }
 
-vector<string> *FileReader::tokenise(string *csv_line, char separator) {
-  vector<string> *tokens = new vector<string>;
+vector<string> FileReader::tokenise(const string &csv_line, char separator) {
+  vector<string> tokens{};
   signed int start, end;
-  start = csv_line->find_last_not_of(separator, 0);
+  start = csv_line.find_last_not_of(separator, 0);
 
   string token;
 
   do {
-    end = csv_line->find_first_of(separator, start);
+    end = csv_line.find_first_of(separator, start);
     if (end >= 0) {
-      token = csv_line->substr(start, end - start);
+      token = csv_line.substr(start, end - start);
     } else {
-      token = csv_line->substr(start, csv_line->length() - start);
+      token = csv_line.substr(start, csv_line.length() - start);
     }
 
-    tokens->push_back(token);
+    tokens.push_back(token);
     start = end + 1;
   } while (end > 0);
 
--- utils/file_reader.h
+++ utils/file_reader.h
@@ -6,6 +6,6 @@ using namespace std;
 class FileReader {
 private:
 public:
-  static vector<string> read_file(string path);
-  static vector<string> *tokenise(string *csvLine, char separator);
+  static vector<string> read_file(const string &path);
+  static vector<string> tokenise(const string &csvLine, char separator);
 };
