c50a748cbd21a79bf5b49273c3fce1663d2205db
Artem3124 <mr.zhukovsky02@gmail.com>
2024-12-17 12:13:45 +0100

    feat(temp_point): mapping func created
    Need to be tested

--- core/temperature_point.cpp
+++ core/temperature_point.cpp
@@ -1,5 +1,7 @@
 #include "./temperature_point.h"
 #include "../utils/file_reader.h"
+#include <cmath>
+#include <string>
 #include <sys/types.h>
 
 TemperaturePoint::TemperaturePoint(eu_location _location, float _temperature, string _date) : 
@@ -8,17 +10,24 @@ TemperaturePoint::TemperaturePoint(eu_location _location, float _temperature, st
 vector<TemperaturePoint> TemparatureDataExtractor::get_temperature_data(const string &path) {
     vector<TemperaturePoint> points{};
     vector<string> rows = FileReader::read_file(path);
-    TemperaturePoint *point = new TemperaturePoint(eu_location::at, 0.0, "error");
 
     for(const string &row : rows) {
         vector<string> tokens = FileReader::tokenise(row, ',');
 
         for (u_int i = 0; i < tokens.size(); ++i) {
-            TemperaturePoint *point 
+            eu_location location = eu_location::uknown;
+            float temperature = -273.15;
+            string date = tokens[i];
+
+            // Need to be tested
+            if (i % 3 == 1) {
+                temperature = stof(tokens[i]);
+                location = static_cast<eu_location>(floor(i / 3));
+            }
+
+            points.emplace_back(location, temperature, date);
         }
     }
-
-    
 
     return points;
 }
