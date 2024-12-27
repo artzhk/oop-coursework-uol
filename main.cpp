c75618621356200b492182a89b941a88ce4604d3
Artem3124 <mr.zhukovsky02@gmail.com>
2024-12-28 00:26:04 +0100

    chore(main)

--- main.cpp
+++ main.cpp
@@ -1,15 +1,45 @@
-#include "core/core.h"
+#include <iostream>
+#include <termios.h>
+#include <unistd.h>
 
 using namespace std;
 
 int main() {
-  Menu *menu = new Menu();
-  OrderBook *order_book = new OrderBook("./datasets/dataset.csv");
-  Core core{menu, order_book};
-  Application app{&core};
+    termios oldt, newt;
 
-  while (1) {
-    app.run();
-  }
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+
+    char input[5];
+
+    read(STDIN_FILENO, input, 3);
+
+    for (const char &c : input) {
+        cout << "You pressed: " << c << endl;
+    }
+
+    // read(STDIN_FILENO, next, 2);
+
+    // if (*input == '\x1b') {
+    //     cout << "Arrow key?" << '\\\\' << input << endl;
+    //     // cout << "Next: " << next[0] << next[1] << endl;
+    // }
+
+
+    // tcgetattr(STDIN_FILENO, &oldt);
+    // newt = oldt;
+    // newt.c_lflag &= ~(ICANON | ECHO);
+    // tcsetattr(STDIN_FILENO, TCSADRAIN, &newt);
+
+    // char button1;
+    // read(STDIN_FILENO, &button1, 2);
+    // write(STDIN_FILENO, &button1, 1);
+    // cout << "\n" << endl;
+
+    return 0;
+
 }
 
